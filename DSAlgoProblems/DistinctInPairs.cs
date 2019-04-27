36c7e2ec3a5bb7c9b4bd0c85f58cc85ab1b26e75
Agrawal, Saurabh <Agrawal.Saurabh@bcg.com>
2019-04-27 14:34:14 +0530

    added code for distinct triplets

---
+++ DSAlgoProblems/DistinctInPairs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSAlgoProblems
+{
+    class DistinctInPairs
+    {
+        static void Main2(string[] args)
+        {
+            var number = singleNumber(new List<int> { 1, 2, 2, 3, 1 });
+            Console.WriteLine(number);
+            Console.Read();
+        }
+
+        public static int singleNumber(List<int> A)
+        {
+            if(A.Count ==0)
+            {
+                throw new Exception("Empty Array");
+            }
+           A.Sort();
+            for(int i=0;i<A.Count;i++)
+            {
+                if(i==A.Count-1)
+                {
+                    return A[i];
+                }
+                if((A[i] ^ A[i+1] )== 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    return A[i];
+                }
+                
+            }
+            throw new Exception("No duplicate element found");
+        }
+    }
+}
---
+++ DSAlgoProblems/DistinctInTriplets.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSAlgoProblems
+{
+    class DistinctInTriplets
+    {
+        static void Main(string[] args)
+        {
+            var number = singleNumber(new List<int> { 1, 2, 2, 2, 14,14,14,7, 1 ,1,9,9,9});
+            Console.WriteLine(number);
+            Console.Read();
+        }
+
+        public static int singleNumber(List<int> A)
+        {
+            if(A.Count ==0)
+            {
+                throw new Exception("Empty Array");
+            }
+            int result = 0;
+            for(int i=0;i<32;i++)
+            {
+                //find ith bit of the number
+                int sum = 0;
+                foreach (var item in A)
+                {
+                    var iThBit = ((1 << i) & item) >= 1 ? 1 : 0;
+                    sum += iThBit;
+                }
+                if(sum%3!=0)
+                {
+                    result = result | (1 << i);
+                }
+             
+            }
+            return result;
+
+
+
+            throw new Exception("No duplicate element found");
+        }
+    }
+}
